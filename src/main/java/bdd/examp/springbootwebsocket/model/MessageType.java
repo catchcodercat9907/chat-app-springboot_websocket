693a1d71f47f736fed47d95cd9733d9507d80f9d
TQHung <hungqtmta2021@gmail.com>
2021-08-27 09:56:47 +0700

    initial commit

---
+++ src/main/java/bdd/examp/springbootwebsocket/model/MessageType.java
@@ -0,0 +1,5 @@
+package bdd.examp.springbootwebsocket.model;
+
+public enum MessageType {
+    CHAT, LEAVE, JOIN
+}
---
+++ src/test/java/bdd/examp/springbootwebsocket/SpringbootwebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package bdd.examp.springbootwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
